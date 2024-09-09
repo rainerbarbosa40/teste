4158dfdc85fa22f982496e6cd28d50b5e002aab4
rainerbarbosa40 <rainer.ctd@gmail.com>
2024-09-09 13:25:38 -0300

    corrigindo wsgi

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tetste.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teste.settings')
 
 application = get_wsgi_application()
